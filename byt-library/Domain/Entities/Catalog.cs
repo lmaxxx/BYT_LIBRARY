1509fa6111c945e05be33e1249b4361539afdd01
Z1BIX <maksym.holovatenko@assaabloy.com>
2025-11-11 21:27:11 +0100

    Modified Catalog class

--- byt-library/Domain/Entities/Catalog.cs
+++ byt-library/Domain/Entities/Catalog.cs
@@ -5,7 +5,14 @@ public class Catalog
     public string Name { get; set; }
     public Dictionary<string, IResource> resources { get; }
 
+    public Catalog(string name)
+    {
+        Name = name;
+        resources = new Dictionary<string, IResource>();
+    }
+
     public void AddResource(IResource resource)
     {
+        resources.Add(resource.Title, resource);
     }
 }
