f93249dc7e3211c1e88dc32c375919492ffc7eb0
Z1BIX <maxgolovatenko@gmail.com>
2025-12-11 02:32:12 +0200

    Added association for Catalog and IResource and tests for it

---
+++ byt-library/Domain/Exceptions/ResourceIsAlreadyPresentInTheCatalogException.cs
@@ -0,0 +1,9 @@
+﻿namespace byt_library.Domain.Exceptions;
+
+public class ResourceIsAlreadyPresentInTheCatalogException : ArgumentException
+{
+    public ResourceIsAlreadyPresentInTheCatalogException(string message) : base(message)
+    {
+        
+    }
+}
---
+++ byt-library/Domain/Exceptions/ResourceIsNotPresentInTheCatalogException.cs
@@ -0,0 +1,9 @@
+﻿namespace byt_library.Domain.Exceptions;
+
+public class ResourceIsNotPresentInTheCatalogException : ArgumentException
+{
+    public ResourceIsNotPresentInTheCatalogException(string message) : base(message)
+    {
+        
+    }
+}
